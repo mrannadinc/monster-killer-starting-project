223bb21661e58516cbcf315af09e5313fe40c7f1
Halil İbrahim <h.ibrahim2321@gmail.com>
2020-12-06 15:21:15 +0300

    commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,43 @@
 const ATTACK_VALUE  = 10;
+const STRONG_ATTACT_VALUE = 17;
+const MONSTER_ATTACT_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
-
 adjustHealthBars(chosenMaxLife);
 
-function attactHandler() {
-  const damege = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACT_VALUE;
+  }
+  const damege = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damege;
-  if(currentMonsterHealth <= 0) {
+  const monsterDamege = dealPlayerDamage(MONSTER_ATTACT_VALUE);
+  currentPlayerHealth -= monsterDamege;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('You won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lost!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
+    alert('you hava a draw!');
   }
 }
 
-attackBtn.addEventListener(click, attactHandler);
+function attactHandler() {
+  attackMonster('ATTACK')
+}
 
+function strongAttactHandler() {
+  attackMonster('STRONG_ATTACK')
+}
+
+attackBtn.addEventListener('click', attactHandler);
+strongAttackBtn.addEventListener('click', strongAttactHandler);
 
 
 
