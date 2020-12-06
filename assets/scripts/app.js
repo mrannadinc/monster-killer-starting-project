8559851b04829235bbc3d2a029651c74041603a6
Halil İbrahim <h.ibrahim2321@gmail.com>
2020-12-06 17:41:13 +0300

    commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,18 +6,39 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+  resetGame(chosenMaxLife);
+}
+
 function endRound() {
+  const initialPlayerHealth = currentPlayerHealth;
   const playerDamege = dealPlayerDamage(MONSTER_ATTACT_VALUE);
   currentPlayerHealth -= playerDamege;
+
+  if (currentPlayerHealth <=0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    setPlayerHealth(initialPlayerHealth);
+    alert('You would be dead but the bonus life saced you!');
+  }
+
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('You won!');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('You lost!');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
     alert('you hava a draw!');
+  }
+
+  if (currentPlayerHealth <= 0 || currentMonsterHealth <= 0) {
+    reset();
   }
 }
 
@@ -57,19 +78,3 @@ function healPlayerHandler() {
 attackBtn.addEventListener('click', attactHandler);
 strongAttackBtn.addEventListener('click', strongAttactHandler);
 healBtn.addEventListener('click', healPlayerHandler);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
